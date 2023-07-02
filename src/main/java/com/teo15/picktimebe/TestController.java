cb21a2ba388311e970c3c176e67e0db01f93ff34
devMtn30 <devMtn30@gmail.com>
2023-07-02 20:01:51 +0900

    feat: CD 테스트용 컨트롤러 추가

--- src/main/java/com/teo15/picktimebe/TestController.java
+++ src/main/java/com/teo15/picktimebe/TestController.java
@@ -3,6 +3,7 @@ package com.teo15.picktimebe;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,10 @@ public class TestController {
 //        var response = cartService.AddToCart(token, request);
         var response = "test resonse";
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/api/v1/test")
+    public ResponseEntity<String> Test() {
+        return new ResponseEntity<>("OK", HttpStatus.OK);
     }
 }
