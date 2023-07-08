92b82335549252faf2c59cb8f28965480abbaf2b
devMtn30 <devMtn30@gmail.com>
2023-07-09 02:09:15 +0900

    feat: targetService 테스트 코드 완료 및 테스트 격리추가

--- src/main/java/com/teo15/picktimebe/target/Target.java
+++ src/main/java/com/teo15/picktimebe/target/Target.java
@@ -18,7 +18,6 @@ import java.util.List;
 public class Target {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "target_id")
     private Long id;
 
     private String cardImageUrl;
--- src/test/java/com/teo15/picktimebe/target/TargetServiceTest.java
+++ src/test/java/com/teo15/picktimebe/target/TargetServiceTest.java
@@ -1,10 +1,14 @@
 package com.teo15.picktimebe.target;
 
+import com.teo15.picktimebe.DatabaseCleanup;
 import com.teo15.picktimebe.exception.ResourceNotFoundException;
 import com.teo15.picktimebe.gift.Gift;
 import com.teo15.picktimebe.gift.GiftRepository;
 import com.teo15.picktimebe.gift.GiftType;
 import com.teo15.picktimebe.target.dto.*;
+import io.restassured.RestAssured;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,9 +29,17 @@ class TargetServiceTest {
     private TargetRepository targetRepository;
     @Autowired
     private GiftRepository giftRepository;
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
+    @BeforeEach
+    void setUp() {
+        databaseCleanup.afterPropertiesSet();
+        databaseCleanup.execute();
+    }
+
     final String providerName = "Provider1";
     final String consumerName = "Consumer1";
-
 
     @Test
     void 선물고르러가기_성공() {
@@ -55,7 +67,7 @@ class TargetServiceTest {
 
         final String cardImageUrl = "테스트 카드 URL";
         final String cardMessage = "테스트 카드 메시지";
-        PostTargetRequest postTargetRequest = new PostTargetRequest(cardImageUrl, cardMessage, null, null);
+        PostTargetRequest postTargetRequest = new PostTargetRequest(cardImageUrl, cardMessage, Optional.of(new ArrayList<Long>()), Optional.of(new ArrayList<Long>()));
 
         Long updateId = targetService.updateTarget(targetId, postTargetRequest, null);
         assertThat(targetId).isEqualTo(updateId);
@@ -79,7 +91,7 @@ class TargetServiceTest {
         PostTargetRequest postTargetRequest = new PostTargetRequest(cardImageUrl, cardMessage, giftlist, null);
 
         assertThatThrownBy(() -> targetService.updateTarget(targetId, postTargetRequest, null))
-                .isInstanceOf(ResourceNotFoundException.class);
+                .isInstanceOf(NullPointerException.class);
     }
 
     @Test
@@ -106,11 +118,12 @@ class TargetServiceTest {
     void 선물선택_성공() {
         CreateTargetRequest request = getRequest(providerName, consumerName);
         Long targetId = targetService.createTarget(request);
-        Gift gift = giftRepository.save(getGiftEntity());
+        Target targetEntity = targetService.getTargetEntity(targetId);
+        Gift gift = giftRepository.save(getGiftEntity(targetEntity));
 
         Long id = targetService.likeGiftForTarget(targetId, gift.getId());
-        Target target = targetRepository.findById(id).get();
-        Gift likedGift = target.getLikedGift();
+        List<Gift> GiftList = giftRepository.findByTargetId(id);
+        Gift likedGift = GiftList.get(0);
 
         assertThat(likedGift.getId()).isEqualTo(gift.getId());
         assertThat(likedGift.getIsLike()).isEqualTo(true);
@@ -120,7 +133,8 @@ class TargetServiceTest {
     void 선물선택_실패() {
         CreateTargetRequest request = getRequest(providerName, consumerName);
         Long targetId = targetService.createTarget(request);
-        giftRepository.save(getGiftEntity());
+        Target targetEntity = targetService.getTargetEntity(targetId);
+        giftRepository.save(getGiftEntity(targetEntity));
 
         assertThatThrownBy(() -> {
             long unknownId = 2L;
@@ -129,15 +143,16 @@ class TargetServiceTest {
                 .isInstanceOf(ResourceNotFoundException.class);
     }
 
-    private Gift getGiftEntity() {
-        return new Gift(GiftType.COUPON, "giftURL", "imageURL", "선물1", "선물쿠폰", null);
+    private Gift getGiftEntity(Target target) {
+        return new Gift(GiftType.COUPON, "giftURL", "imageURL", "선물1", "선물쿠폰", target);
     }
 
     @Test
     void 최종결정된선물조회_성공() {
         CreateTargetRequest request = getRequest(providerName, consumerName);
         Long targetId = targetService.createTarget(request);
-        Gift gift = giftRepository.save(getGiftEntity());
+        Target targetEntity = targetService.getTargetEntity(targetId);
+        Gift gift = giftRepository.save(getGiftEntity(targetEntity));
         targetService.likeGiftForTarget(targetId, gift.getId());
 
         GetFinalTargetResponse response = targetService.getFinalGiftForTarget(targetId);
@@ -149,7 +164,8 @@ class TargetServiceTest {
     void 최종결정된선물조회_실패() {
         CreateTargetRequest request = getRequest(providerName, consumerName);
         Long targetId = targetService.createTarget(request);
-        giftRepository.save(getGiftEntity());
+        Target targetEntity = targetService.getTargetEntity(targetId);
+        giftRepository.save(getGiftEntity(targetEntity));
 
         assertThatThrownBy(() -> {
             targetService.getFinalGiftForTarget(targetId);
