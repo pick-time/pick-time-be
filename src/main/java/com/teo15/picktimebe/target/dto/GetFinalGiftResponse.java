0df4aa55cd41bc20e20b9c318cf800df70ded8d8
devMtn30 <devMtn30@gmail.com>
2023-07-16 22:12:05 +0900

    refactor: Target 최종 결정된 선물 조회 API response값 변경(선물 url 추가)

--- src/main/java/com/teo15/picktimebe/target/dto/GetFinalGiftResponse.java
+++ src/main/java/com/teo15/picktimebe/target/dto/GetFinalGiftResponse.java
@@ -9,9 +9,11 @@ import lombok.*;
 public class GetFinalGiftResponse {
     private String giftTitle;
     private String giftImageUrl;
+    private String giftUrl;
 
-    public GetFinalGiftResponse(String giftTitle, String giftImageUrl) {
+    public GetFinalGiftResponse(String giftTitle, String giftImageUrl, String giftUrl) {
         this.giftTitle = giftTitle;
         this.giftImageUrl = giftImageUrl;
+        this.giftUrl = giftUrl;
     }
 }
