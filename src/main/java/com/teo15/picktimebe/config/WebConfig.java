d6d81eec7f3558da906bd093e440c60a8db58869
comody <comody224@gmail.com>
2023-07-22 11:09:24 +0900

    bug: configuration 어노테이션 추가

--- src/main/java/com/teo15/picktimebe/config/WebConfig.java
+++ src/main/java/com/teo15/picktimebe/config/WebConfig.java
@@ -3,7 +3,7 @@ package com.teo15.picktimebe.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
