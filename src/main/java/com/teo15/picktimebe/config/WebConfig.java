2e18490d4fa4de90668d6738ee89bd7014bb3d8e
devMtn30 <devMtn30@gmail.com>
2023-07-22 19:31:29 +0900

    fix: CORS 설정 변경 및 프론트 로컬 개발환경에서 proxy를 사용하지 않고 개발할 수 있도록 변경

--- src/main/java/com/teo15/picktimebe/config/WebConfig.java
+++ src/main/java/com/teo15/picktimebe/config/WebConfig.java
@@ -5,11 +5,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://pick-time.vercel.app")
+                .allowedOriginPatterns("*")
                 .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
                 .maxAge(3000);
     }
 }
